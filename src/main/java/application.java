b581b2d3b7cd1469fa1eca1a95ab8ec747009905
stephen.wille <stephen.wille@gmail.com>
2016-06-19 13:00:02 -0700

    init proxy

---
+++ src/main/java/application.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import proxy.gumball.GumballMonitorTestDrive;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class application {
+    public static void main(String[] args) {
+        System.out.println("I am alive!");
+        GumballMonitorTestDrive.main();
+
+    }
+}
