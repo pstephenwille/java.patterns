d6bde08f6adf4e142fe6d893272f5cc18aa8490c
stephen.wille <stephen.wille@gmail.com>
2016-07-02 13:46:18 -0700

    init state

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,7 +1,16 @@
 package main.java;
 
+import state.gumball.GumballMachineTestDrive;
+
 public class application {
     public static void main(String[] args) {
         System.out.println("I am alive!");
+        /*
+        * State:
+        * Allows an object to alter its behavior when its internal state changes.
+        * State behavior is encapsulated and decoupled from the context via interfaces.
+        *
+        * Same pattern as Strategy.*/
+        GumballMachineTestDrive.main(null);
     }
 }
