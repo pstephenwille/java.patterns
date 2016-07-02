d1b02bcf1f990645b350d3dec12fa0f1774faa8c
stephen.wille <stephen.wille@gmail.com>
2016-07-02 08:46:17 -0700

    init::strategy

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,7 +1,22 @@
 package main.java;
 
+import strategy.RubberDuck;
+
 public class application {
+    static RubberDuck rubberDuck = new RubberDuck();
+
     public static void main(String[] args) {
         System.out.println("I am alive!");
+        rubberDuck.display();
+    /* Strategy:
+    * The context is the RubberDuck.
+    *
+    * A ducks flying behavior is encapsulated in a 'flyBehavior' class;
+    * in this case, the 'FlyNoWay' class.
+    * This makes it's flying behavior interchangeable and decoupled from itself,
+    *
+    * Utilizing the 'flyBehavior' interface we are...
+    * favoring compostion over inheritance.*/
+        rubberDuck.performFly();
     }
 }
