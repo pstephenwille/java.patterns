b3a26bc39763db47e6dd07d12187206d03692c3b
stephen.wille <stephen.wille@gmail.com>
2016-07-04 13:40:27 -0700

    init Observer

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,7 +1,30 @@
 package main.java;
 
+import observer.weatherobservable.CurrentConditionsDisplay;
+import observer.weatherobservable.ForecastDisplay;
+import observer.weatherobservable.StatisticsDisplay;
+import observer.weatherobservable.WeatherData;
+
 public class application {
-    public static void main(String[] args) {
-        System.out.println("I am alive!");
-    }
+
+	public static void main(String[] args) {
+		/*
+		* Observer
+		* The observable (weatherData) maintains a list of observers,
+		* and notifies them of any changes via its update method.
+		*
+		* Observers (currentConditions) are instantiated with the observable,
+		* and register themselves with it.  All observers are notified of changes from the
+		* observers update method.
+		*
+		* Behavioral */
+		WeatherData weatherData = new WeatherData();
+		CurrentConditionsDisplay currentConditions = new CurrentConditionsDisplay(weatherData);
+		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+	}
 }
