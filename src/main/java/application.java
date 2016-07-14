9e37c3b42279f86a726b93edd87a9b153a24894b
stephen.wille <stephen.wille@gmail.com>
2016-07-14 07:23:31 -0700

    init:singleton

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,7 +1,22 @@
 package main.java;
 
+import singleton.subclass.CoolerSingleton;
+import singleton.subclass.HotterSingleton;
+import singleton.subclass.Singleton;
+
 public class application {
     public static void main(String[] args) {
         System.out.println("I am alive!");
+
+		SingletonTestDrive.main(null);
+    }
+
+	public static class SingletonTestDrive {
+        public static void main(String[] args) {
+            Singleton foo = CoolerSingleton.getInstance();
+            Singleton bar = HotterSingleton.getInstance();
+            System.out.println(foo);
+            System.out.println(bar);
+         }
     }
 }
